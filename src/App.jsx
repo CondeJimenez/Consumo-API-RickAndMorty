6fe06a5eab2f0b542442cce51fb8662c28c9e08f
KevinConde <conde.jimenez@hotmail.com>
2023-04-30 20:11:23 -0600

    Co-authored-by: Idalia Aranda <IdaliaAT@users.noreply.github.com> Co-authored-by: Eugenia Ramirez(EU) <kenyanarkt@gmail.com>

--- src/App.jsx
+++ src/App.jsx
@@ -2,20 +2,21 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  const [infoCharacter, setInfoCharacter] = useState({});
+  const [infoCharacter, setInfoCharacter] = useState();
   const [idCharacter, setIdCharacter] = useState(1);
+  
 
   useEffect(() => {
     fetch(`https://rickandmortyapi.com/api/character/${idCharacter}`)
       .then((res) => res.json())
-      .then((character) => setInfoCharacter(character));
-  },[idCharacter]);
+      .then((character) => setInfoCharacter(character))
+    
+  }, [idCharacter]);
+  // console.log(infoCharacter);
 
   const handleChangeCharacterId = () => {
     setIdCharacter(idCharacter + 1);
   };
-
-  console.log("characterS", infoCharacter);
 
   // Muestrame un character de la API | Listo
   // muestra su nombre | Listo
@@ -23,22 +24,20 @@ function App() {
   // muestra su genero | Listo
   // muestra su especie | Listo
   // muestralo los elementos centrados y alineados al centro
-
   return (
-    <section className="card" >
-
-      <h1>Nombre: {infoCharacter.name}</h1>
-      <h1>Genero: {infoCharacter.gender} </h1>
-      <h1>Especie: {infoCharacter.species}</h1>
+    <section className="card">
+      <h1>Nombre: {infoCharacter?.name}</h1>
+      <h1>Genero: {infoCharacter?.gender} </h1>
+      <h1>Especie: {infoCharacter?.species}</h1>
       <h1 className="img">
-        Imagen: 
-        <img src={infoCharacter.image} alt={`imagen de ${infoCharacter.name}`}/>
+        Imagen:
+        <img
+          src={infoCharacter?.image}
+          alt={`imagen de ${infoCharacter?.name}`}
+        />
       </h1>
-      {/* TE DEJO LA IMG NORMITA, ok Eu ,  Muchas gracias Eu */}
-      {/* YOU CAN DO IT */}  
+
       <button onClick={handleChangeCharacterId}>Cambiar Personaje</button>
-      
-     
     </section>
   );
 }
